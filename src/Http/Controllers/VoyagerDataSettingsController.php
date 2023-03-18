b9696089e1352208dde7771426fe3395c6e98802
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2023-03-18 11:23:46 +0530

    Undefined method

--- src/Http/Controllers/VoyagerDataSettingsController.php
+++ src/Http/Controllers/VoyagerDataSettingsController.php
@@ -2,10 +2,22 @@
 
 namespace Joy\VoyagerDataSettings\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Joy\VoyagerDataSettings\Http\Traits\CrudActions;
 use Joy\VoyagerCore\Http\Controllers\Controller as TCGVoyagerController;
 
 class VoyagerDataSettingsController extends TCGVoyagerController
 {
     use CrudActions;
+
+    protected function getSlug(Request $request)
+    {
+        if (isset($this->slug)) {
+            $slug = $this->slug;
+        } else {
+            $slug = explode('.', $request->route()->getName())[1];
+        }
+
+        return $slug;
+    }
 }
